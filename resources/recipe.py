0ee2e74b2042dd8396bcda32080d37eb0259fbeb
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2022-01-02 15:59:26 +1100

    put, delete resources

--- resources/recipe.py
+++ resources/recipe.py
@@ -30,10 +30,11 @@ class RecipeListResource(Resource):
 
         return recipe.data, HTTPStatus.CREATED
 
+
 class RecipeResource(Resource):
 
     def get(self, recipe_id):
-        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id and recipe.is_publish ==True), None)
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id and recipe.is_publish == True), None)
 
         if recipe is None:
             return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
@@ -57,3 +58,24 @@ class RecipeResource(Resource):
         return recipe.data, HTTPStatus.OK
 
 
+class RecipePublic(Resource):
+
+    def put(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+
+        recipe.is_publish = True
+
+        return {}, HTTPStatus.NO_CONTENT
+
+    def delete(self, recipe_id):
+        recipe = next((recipe for recipe in recipe_list if recipe.id == recipe_id), None)
+
+        if recipe is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+
+        recipe.is_publish = False
+
+        return {}, HTTPStatus.NO_CONTENT

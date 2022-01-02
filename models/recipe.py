106cfb71139888594f333c789472308131523192
Chad Duffey <7192541+chadduffey@users.noreply.github.com>
2022-01-03 06:19:37 +1100

    typo

--- models/recipe.py
+++ models/recipe.py
@@ -12,7 +12,7 @@ def get_last_id():
 class Recipe:
 
     def __init__(self, name, description, num_of_servings, cook_time, directions):
-        self.id == get_last_id()
+        self.id = get_last_id()
         self.name = name
         self.description = description
         self.num_of_servings = num_of_servings
@@ -30,5 +30,3 @@ class Recipe:
             'cook_time': self.cook_time,
             'directions': self.directions
         }
-
-
